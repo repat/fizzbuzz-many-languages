c69e4dde8f1ddc3b1473d79d89258df2694866c7
repat <repat@repat.de>
2013-10-18 10:56:00 +0200

    fixed java and added c

---
+++ c/fizzbuzz1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+  int i = 1;
+
+  for (i = 1; i < 100; i++) {
+    if (i % 15 == 0)
+      printf("Fizzbuzz ");
+    else if (i % 5 == 0)
+      printf("Buzz ");
+    else if (i % 3 == 0)
+      printf("Fizz ");
+    else
+      printf("%d ",i);
+  }
+}
--- java/Fizzbuzz1.java
+++ java/Fizzbuzz1.java
@@ -2,12 +2,12 @@ public class Fizzbuzz {
 
 	public static void main(String[] args) {
 		for (int i = 1; i < 100; i++) {
-			if (i % 3 == 0)
-				System.out.println("Fizz");
+			if (i % 15 == 0)
+				System.out.println("Fizzbuzz");
 			else if (i % 5 == 0)
 				System.out.println("Buzz");
-			else if (i % 15 == 0)
-				System.out.println("Fizzbuzz");
+			else if (i % 3 == 0)
+				System.out.println("Fizz");
 			else
 				System.out.println(i);
 		}
