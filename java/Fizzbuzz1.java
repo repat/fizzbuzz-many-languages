49a3a6ca80de497557a1eae76f285036e3202671
repat <repat@repat.de>
2014-01-10 11:04:45 +0100

    groovy

--- java/Fizzbuzz1.java
+++ java/Fizzbuzz1.java
@@ -1,7 +1,7 @@
 public class Fizzbuzz {
 
 	public static void main(String[] args) {
-		for (int i = 1; i < 100; i++) {
+		for (int i = 1; i <= 100; i++) {
 			if (i % 15 == 0)
 				System.out.println("Fizzbuzz");
 			else if (i % 5 == 0)
